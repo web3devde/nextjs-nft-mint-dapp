49954b00cd7a5ea3bc8372ccd40ae15c17a3ad9d
kjmczk <vollgong@gmail.com>
2022-02-28 12:49:30 +0900

    Add text for social tipping

--- components/Team.tsx
+++ components/Team.tsx
@@ -25,6 +25,10 @@ export default function Team() {
           </span>
         </a>
       </p>
+      <p className="mt-6">
+        Is this dApp useful? I am gladly accepting social tipping at
+        0xBD0aF937F03cb1F2C2233d36d2D7cC91AE9a916b
+      </p>
     </div>
   );
 }
