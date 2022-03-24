cf62b103449f11f4a7b813c69f2e216ffe0cda0f
kjmczk <vollgong@gmail.com>
2022-03-24 15:31:52 +0900

    Use `parseEther()` & `toBigInt()` to calc Wei

--- components/Minting.tsx
+++ components/Minting.tsx
@@ -18,12 +18,17 @@ export default function Minting() {
   const [isMinting, setIsMinting] = useState(false);
   const [mintAmount, setMintAmount] = useState(1);
 
-  async function claimNFTs() {
+  const networkName =
+    process.env.NODE_ENV === 'production'
+      ? `${process.env.NEXT_PUBLIC_NETWORK_NAME} Mainnet`
+      : `${process.env.NEXT_PUBLIC_NETWORK_NAME} Testnet`;
+
+  async function mintNFTs() {
     if (account && ethereumProvider) {
-      const cost = process.env.NEXT_PUBLIC_WEI_COST;
-      const gasLimit = process.env.NEXT_PUBLIC_GAS_LIMIT;
-      const totalCostWei = (Number(cost) * mintAmount).toString();
-      const totalGasLimit = (Number(gasLimit) * mintAmount).toString();
+      const totalMintCost = (
+        Number(process.env.NEXT_PUBLIC_MINT_COST) * mintAmount
+      ).toString();
+      const totalWei = ethers.utils.parseEther(totalMintCost).toBigInt();
       setMessage('');
       setIsPending(true);
       try {
@@ -37,8 +42,7 @@ export default function Minting() {
           signer
         );
         const transaction = await contract.mint(mintAmount, {
-          value: totalCostWei,
-          gasLimit: totalGasLimit.toString(),
+          value: totalWei,
         });
         setIsPending(false);
         setIsMinting(true);
@@ -65,7 +69,7 @@ export default function Minting() {
   }
 
   function incrementMintAmount() {
-    if (mintAmount < 10) {
+    if (mintAmount < Number(process.env.NEXT_PUBLIC_MAX_MINT_AMOUNT_PER_TXN)) {
       setMintAmount(mintAmount + 1);
     }
   }
@@ -74,19 +78,13 @@ export default function Minting() {
     if (!active) {
       setConnErrMsg('Not connected to your wallet.');
     } else {
-      if (chainId?.toString() !== process.env.NEXT_PUBLIC_NETWORK_ID) {
-        setConnErrMsg(
-          `Change the network to ${
-            process.env.NODE_ENV === 'production'
-              ? `${process.env.NEXT_PUBLIC_NETWORK_NAME} Mainnet`
-              : `${process.env.NEXT_PUBLIC_NETWORK_NAME} Testnet`
-          }.`
-        );
+      if (chainId?.toString() !== process.env.NEXT_PUBLIC_CHAIN_ID) {
+        setConnErrMsg(`Change the network to ${networkName}.`);
       } else {
         setConnErrMsg('');
       }
     }
-  }, [active, chainId]);
+  }, [active, chainId, networkName]);
 
   useEffect(() => {
     async function fetchTotalSupply() {
@@ -102,7 +100,7 @@ export default function Minting() {
 
     if (
       ethereumProvider &&
-      chainId?.toString() === process.env.NEXT_PUBLIC_NETWORK_ID
+      chainId?.toString() === process.env.NEXT_PUBLIC_CHAIN_ID
     ) {
       fetchTotalSupply();
 
@@ -115,115 +113,111 @@ export default function Minting() {
     <>
       <h2 className="text-4xl mb-4">Minting</h2>
 
-      <div className="space-y-4 mb-4">
-        <div className="bg-gray-800 border border-t-red-300 border-r-blue-300 border-b-green-300 border-l-yellow-300 rounded p-8 space-y-4">
-          <div className="text-3xl font-bold text-center">
-            {totalSupply} / {process.env.NEXT_PUBLIC_MAX_SUPPLY}
-          </div>
-          <div className="text-center">
-            <p className="text-xl">{`${process.env.NEXT_PUBLIC_DISPLAY_COST} ${process.env.NEXT_PUBLIC_CHAIN} per 1 NFT`}</p>
-            <p>(excluding gas fees)</p>
-          </div>
-          <div className="flex justify-center items-center space-x-4">
-            <IconContext.Provider value={{ size: '1.5em' }}>
-              <button
-                type="button"
-                className={
-                  mintAmount === 1 ? 'text-gray-500 cursor-default' : ''
-                }
-                onClick={decrementMintAmount}
-                disabled={false}
-              >
-                <FaMinusCircle />
-              </button>
-              <span className="text-xl">{mintAmount}</span>
-              <button
-                type="button"
-                className={
-                  mintAmount === 10 ? 'text-gray-500 cursor-default' : ''
-                }
-                onClick={incrementMintAmount}
-                disabled={false}
-              >
-                <FaPlusCircle />
-              </button>
-            </IconContext.Provider>
-          </div>
+      <div className="bg-gray-800 border border-t-red-300 border-r-blue-300 border-b-green-300 border-l-yellow-300 rounded p-8 space-y-4">
+        <div className="text-3xl font-bold text-center">
+          {totalSupply} / {process.env.NEXT_PUBLIC_MAX_SUPPLY}
+        </div>
 
-          <div className="flex justify-center">
-            {active && !connErrMsg ? (
-              <>
-                {isPending || isMinting ? (
-                  <button
-                    type="button"
-                    className="flex justify-center items-center rounded px-4 py-2 bg-red-700 font-bold w-40 cursor-not-allowed"
-                    disabled
-                  >
-                    <svg
-                      className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                    >
-                      <circle
-                        className="opacity-25"
-                        cx="12"
-                        cy="12"
-                        r="10"
-                        stroke="currentColor"
-                        strokeWidth="4"
-                      ></circle>
-                      <path
-                        className="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                      ></path>
-                    </svg>
-                    {isPending && 'Pending'}
-                    {isMinting && 'Minting'}
-                    {!isPending && !isMinting && 'Processing'}
-                  </button>
-                ) : (
-                  <button
-                    type="button"
-                    className={`rounded px-4 py-2 bg-blue-700 hover:bg-blue-600 font-bold w-40`}
-                    onClick={claimNFTs}
-                  >
-                    Mint
-                  </button>
-                )}
-              </>
-            ) : (
-              <button
-                type="button"
-                className={`rounded px-4 py-2 bg-gray-700 font-bold w-40 cursor-not-allowed`}
-                disabled={true}
-                onClick={claimNFTs}
-              >
-                Mint
-              </button>
-            )}
-          </div>
+        <div className="text-center">
+          <p className="text-xl">
+            Total price:{' '}
+            {Number(process.env.NEXT_PUBLIC_MINT_COST) * mintAmount}
+          </p>
+          <p className="text-sm text-gray-400">(excluding gas fees)</p>
+        </div>
 
-          {message && (
-            <div className="text-green-500 text-center">{message}</div>
-          )}
-          {connErrMsg && (
-            <div className="text-red-500 text-center">{connErrMsg}</div>
+        <div className="flex justify-center items-center space-x-4">
+          <IconContext.Provider value={{ size: '1.5em' }}>
+            <button
+              type="button"
+              className={mintAmount === 1 ? 'text-gray-500 cursor-default' : ''}
+              onClick={decrementMintAmount}
+              disabled={false}
+            >
+              <FaMinusCircle />
+            </button>
+            <span className="text-xl">{mintAmount}</span>
+            <button
+              type="button"
+              className={
+                mintAmount ===
+                Number(process.env.NEXT_PUBLIC_MAX_MINT_AMOUNT_PER_TXN)
+                  ? 'text-gray-500 cursor-default'
+                  : ''
+              }
+              onClick={incrementMintAmount}
+              disabled={false}
+            >
+              <FaPlusCircle />
+            </button>
+          </IconContext.Provider>
+        </div>
+
+        <div className="flex justify-center">
+          {active && !connErrMsg ? (
+            <>
+              {isPending || isMinting ? (
+                <button
+                  type="button"
+                  className="flex justify-center items-center rounded px-4 py-2 bg-red-700 font-bold w-40 cursor-not-allowed"
+                  disabled
+                >
+                  <svg
+                    className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                  {isPending && 'Pending'}
+                  {isMinting && 'Minting'}
+                  {!isPending && !isMinting && 'Processing'}
+                </button>
+              ) : (
+                <button
+                  type="button"
+                  className={`rounded px-4 py-2 bg-blue-700 hover:bg-blue-600 font-bold w-40`}
+                  onClick={mintNFTs}
+                >
+                  Mint
+                </button>
+              )}
+            </>
+          ) : (
+            <button
+              type="button"
+              className={`rounded px-4 py-2 bg-gray-700 font-bold w-40 cursor-not-allowed`}
+              disabled={true}
+              onClick={mintNFTs}
+            >
+              Mint
+            </button>
           )}
         </div>
 
-        <div className="space-y-4">
-          <p>
-            Please make sure you are connected to the correct address and the
-            correct network (Polygon Mainnet) before purchasing. The operation
-            cannot be undone after purchase.
-          </p>
-          <p>
-            We have set the gas limit to 285000 to successfully mint your NFT.
-            We recommend that you do not lower the gas limit.
-          </p>
-        </div>
+        {message && <div className="text-green-500 text-center">{message}</div>}
+        {connErrMsg && (
+          <div className="text-red-500 text-center">{connErrMsg}</div>
+        )}
+      </div>
+
+      <div className="text-gray-400 mt-2">
+        Please make sure you are connected to the correct address and the
+        correct network ({networkName}) before purchasing. The operation cannot
+        be undone after purchase.
       </div>
     </>
   );
