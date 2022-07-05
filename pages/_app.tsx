d8d0046312fcaa7ddef263cdafe5a5781bd99531
kjmczk <vollgong@gmail.com>
2022-07-05 18:46:18 +0900

    Update pages for dApp v2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { Web3ReactProvider } from '@web3-react/core';
-import { ethers } from 'ethers';
-
-function getLibrary(provider: any) {
-  return new ethers.providers.Web3Provider(provider);
-}
+import { MoralisProvider } from 'react-moralis';
+import { NotificationProvider } from 'web3uikit';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <Component {...pageProps} />
-    </Web3ReactProvider>
+    <MoralisProvider initializeOnMount={false}>
+      <NotificationProvider>
+        <Component {...pageProps} />
+      </NotificationProvider>
+    </MoralisProvider>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,31 @@
-import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import type { NextPage } from 'next';
 
 import Layout from '../components/Layout';
 import Prose from '../components/Prose';
-import Minting from '../components/Minting';
+import Mint from '../components/Mint';
 import Faq from '../components/Faq';
-import Roadmap from '../components/Roadmap';
 import Team from '../components/Team';
-import projectConfig from '../config/projectConfig';
+import Roadmap from '../components/Roadmap';
 import topImage from '../public/assets/1920x600.png';
+import contractConfig from '../config/contract-config.json';
 
 const Home: NextPage = () => {
+  const { nftName } = contractConfig;
+
   return (
     <Layout>
       <Head>
-        <title>{projectConfig.nftName}</title>
+        <title>{nftName}</title>
       </Head>
 
-      <Image src={topImage} alt={projectConfig.nftName} />
+      <Image src={topImage} alt={nftName} />
 
-      <div className="bg-gray-800 py-8">
+      <div className="bg-gray-800 py-16">
         <Prose>
-          <h1 className="text-5xl font-bold mb-2">{projectConfig.nftName}</h1>
+          <h1 className="text-5xl font-bold mb-4">{nftName}</h1>
+
           <p className="text-xl">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
@@ -35,25 +38,25 @@ const Home: NextPage = () => {
         </Prose>
       </div>
 
-      <div className="py-8">
+      <div className="py-16">
         <Prose>
-          <Minting />
+          <Mint />
         </Prose>
       </div>
 
-      <div className="bg-gray-800 py-8">
+      <div className="bg-gray-800 py-16">
         <Prose>
           <Faq />
         </Prose>
       </div>
 
-      <div className="py-8">
+      <div className="py-16">
         <Prose>
           <Roadmap />
         </Prose>
       </div>
 
-      <div className="bg-gray-800 py-8">
+      <div className="bg-gray-800 py-16">
         <Prose>
           <Team />
         </Prose>
